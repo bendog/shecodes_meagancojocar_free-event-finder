f305f581b54ae334b1be279b8d13478d7db1d871
Ben Fitzhardinge <ben@fitzhardinge.net>
2019-09-15 09:52:41 +0800

    setup the local environment

--- eventFinderProject/settings.py
+++ eventFinderProject/settings.py
@@ -74,14 +74,21 @@ WSGI_APPLICATION = 'eventFinderProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'meagancojocar', # you will need to create this db
+#         'USER': 'meagancojocar', # enter your user name here
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'meagancojocar', # you will need to create this db
-        'USER': 'meagancojocar', # enter your user name here
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
     }
 }
 
